00dd0ce1e3272dad9910fe1f1cf24e4e8da8fedb
Илья Ватютов <vatut007@ya.ru>
2026-02-09 21:19:18 +0300

    add base app folders

---
+++ src/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_title: str = 'Сервис сокращения ссылок'
+    description: str = 'Это апи сервиса сокращения ссыллок'
+    database_url: str = 'sqlite+sqlite3:./fastapi.db'
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+from api.routers import main_router
+from core.config import settings
+
+app = FastAPI(title=settings.app_title,
+              description=settings.description)
+
+app.include_router(main_router)
